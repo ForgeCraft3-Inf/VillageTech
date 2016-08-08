dba7496a8d290d572d98714f30557960cb3eb1fe
LimpLungs <xlaxlimplungs@gmail.com>
2016-08-08 16:28:06 -0500

    initial cmmit

---
+++ src/main/java/com/limplungs/villagetech/VillageTech.java
@@ -0,0 +1,18 @@
+package com.limplungs.villagetech;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = VillageTech.MODID, version = VillageTech.VERSION)
+public class VillageTech
+{
+    public static final String MODID = "villagetech";
+    public static final String VERSION = "1.0dev1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
